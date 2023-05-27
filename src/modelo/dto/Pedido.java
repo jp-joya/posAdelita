5e4bb86ab7513c75ff1f47e8c269b8735b735622
jp-joya <jp.joya@hotmail.com>
2023-05-26 22:44:29 -0500

    n

---
+++ src/modelo/dto/Pedido.java
@@ -0,0 +1,13 @@
+package modelo.dto;
+
+import java.util.ArrayList;
+
+public class Pedido {
+	
+	private int numero;
+	private ArrayList<Orden> pedido;
+	
+	public void agregarOrden() {
+		pedido.add();
+	}
+}
